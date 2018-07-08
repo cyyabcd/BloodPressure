9d6c6ffc7f6c3172b719c397f0afbc7167f20578
ChenYuyan <603507745@qq.com>
2018-07-08 11:12:15 +0800

    1

--- BloodPressure/BloodPressure.py
+++ BloodPressure/BloodPressure.py
@@ -1,1 +1,71 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+sequence_length = 28
+input_size = 28
+hidden_size = 128
+num_layers = 2
+
+num_classes = 10
+batch_size = 100
+num_epochs = 20
+learning_rate = 0.1
+
+train_dataset = datasets.MNIST(root = './data/', train = True, transform = transforms.ToTensor(), download = True)
+test_dataset = datasets.MNIST(root = './data/', train = False, transform = transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
+
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first = True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda()
+        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda()
+        out, (h_n, c_n) = self.lstm(x, (h0, c0))
+        out = self.fc(out[:,1,:])
+        return out
+
+rnn = RNN(input_size, hidden_size, num_layers, num_classes)
+rnn.cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(rnn.parameters(), lr = learning_rate)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = Variable(images.view(-1, sequence_length, input_size)).cuda()
+        labels = Variable(labels).cuda()
+        
+        optimizer.zero_grad()
+        outputs = rnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+#        if (i + 1) % 100 == 0:
+#            print(loss.data[0]);
+
+correct = 0
+total = 0
+
+for images, labels in test_loader:
+    images = Variable(images.view(-1, sequence_length, input_size)).cuda()
+    outputs = rnn(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted.cpu() == labels).sum()
+acc = correct / total * 100
+print(correct, total, acc)
 
